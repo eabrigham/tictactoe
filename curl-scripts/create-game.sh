4399b23e35b5d54f3c52f211e80df8d0d15483fb
eabrigham <eabrigham@gmail.com>
2018-06-22 15:59:37 -0400

    Made create-game.sh curl script (and successfully used it)
    Successfully signed in user, got authentication token, and told API
    that my user played a game using the correct JSON format.

---
+++ curl-scripts/create-game.sh
@@ -0,0 +1,17 @@
+
+curl 'https://tic-tac-toe-wdi.herokuapp.com/games' \
+  --include --request POST  \
+     --header 'content-type: application/json' \
+     --header "Authorization: Token token=${TOKEN}" \
+     --data '{
+       "game": {
+         "id": 3,
+         "cells": ["","","","","","","","",""],
+         "over": false,
+         "player_x": {
+           "id": 1,
+           "email": "and@and.com"
+         },
+         "player_o": null
+       }
+     }'
