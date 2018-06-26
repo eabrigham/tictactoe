c7b457b4d8b311d88ce46d9cd7d1b659d0d786ec
eabrigham <eabrigham@gmail.com>
2018-06-26 15:24:46 -0400

    Created gameObj.js and copied relevant code from the AM before reset

--- assets/scripts/game2/game.js
+++ assets/scripts/game2/game.js
@@ -1,8 +1,6 @@
 const setup = require('./setup.js')
 
-
 const testArray1D = ['', '', 'o', 'o', 'x', 'x', 'x', 'o', '']
-
 
 const makeBoard = function () {
   setup.boardToHTML(testArray1D)
---
+++ assets/scripts/game2/gameObj.js
@@ -0,0 +1,9 @@
+const game = {
+  cells: ['', '', 'o', 'o', 'x', 'x', 'x', 'o', ''],
+  currentPlayer: 'x'
+}
+
+// const Game = function (cells, currentPlayer) {
+//   this.cells = cells
+//   this.currentPlayer = currentPlayer
+// }
