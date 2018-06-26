5f296dc7fc0892f98996539e009b957cf8fdaedb
eabrigham <eabrigham@gmail.com>
2018-06-26 14:19:01 -0400

    Currently have my previous (working) version under game.js, and also the newer (not working) version under game/

---
+++ assets/scripts/game/setup.js
@@ -0,0 +1,45 @@
+const game = require('./game.js')
+
+// A function to turn a 9-element array into a 3X3 2D testArray
+const arrayTo2D = function (array) {
+  const arrayBoard = []
+  const boardWidth = 3
+  for (let row = 0; row < boardWidth; row++) {
+    const arrayRow = []
+    for (let col = 0; col < boardWidth; col++) {
+      arrayRow[col] = array[ row * boardWidth + col ]
+    }
+    arrayBoard.push(arrayRow)
+  }
+  return arrayBoard
+}
+
+// Creates an HTML table in the DOM based on a nine-element JS array.
+// Attaches a click listener to each table element.
+const boardToHTML = function (inputArray) {
+  console.log('creating DOM array')
+  // create 3 by 3 JS 2D array
+  const array = arrayTo2D(inputArray)
+  const boardDiv = document.getElementById('game-board')
+  const table = document.createElement('table')
+  for (let row = 0; row < array.length; row++) {
+    const tableRow = document.createElement('tr')
+    for (let col = 0; col < array[row].length; col++) {
+      const tableData = document.createElement('td')
+      // TODO: make a variable to go into the array?
+      const textNode = document.createTextNode(array[row][col])
+      // the number in a 9-element list
+      const listNum = 3 * row + col
+      tableData.setAttribute('id', listNum)
+      $(tableData).click(game.onSquareClick)
+      tableData.appendChild(textNode)
+      tableRow.appendChild(tableData)
+    }
+    table.appendChild(tableRow)
+  }
+  boardDiv.appendChild(table)
+}
+
+module.exports = {
+  boardToHTML
+}
