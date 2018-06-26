e07709804474d9ba40bcebde585f4f7f9111008a
eabrigham <eabrigham@gmail.com>
2018-06-26 15:16:59 -0400

    Fixed bug. Tic tac toe is responsive again!!

--- assets/scripts/game2/game.js
+++ assets/scripts/game2/game.js
@@ -1,11 +1,11 @@
-const user = require('./user.js')
+const setup = require('./setup.js')
 
 
 const testArray1D = ['', '', 'o', 'o', 'x', 'x', 'x', 'o', '']
 
 
 const makeBoard = function () {
-  user.boardToHTML(testArray1D)
+  setup.boardToHTML(testArray1D)
 }
 
 module.exports = {
