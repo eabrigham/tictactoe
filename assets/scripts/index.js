c9d195be183edf539614609f640658e879e5f1ce
eabrigham <eabrigham@gmail.com>
2018-06-27 15:36:53 -0400

    Moved require statment to top of page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,6 +6,7 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events.js')
+const game = require('./game2/game.js')
 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
@@ -13,6 +14,5 @@ $(() => {
   $('#sign-out').on('submit', authEvents.onSignOut)
   $('#change-password').on('submit', authEvents.onChangePassword)
 
-  const game = require('./game2/game.js')
   game.makeBoard()
 })
