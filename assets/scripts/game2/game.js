5092eac6e38f616b0be193ed408a7f65ff79d43b
eabrigham <eabrigham@gmail.com>
2018-06-26 21:58:32 -0400

    Moved new game instantiation from gameObj.js to game.js

--- assets/scripts/game2/game.js
+++ assets/scripts/game2/game.js
@@ -1,8 +1,13 @@
 const setup = require('./setup.js')
 const gameObj = require('./gameObj.js')
 
+const currCells = ['', '', 'o', 'o', 'x', 'x', 'x', 'o', '']
+const currPlayer = 'x'
+
+const currGame = new gameObj.Game(currCells, currPlayer)
+
 const makeBoard = function () {
-  setup.boardToHTML(gameObj.currGame.cells)
+  setup.boardToHTML(currGame.cells)
 }
 
 module.exports = {
--- assets/scripts/game2/gameObj.js
+++ assets/scripts/game2/gameObj.js
@@ -1,15 +1,11 @@
 // For now, I am hard-coding these sample values for the game.
 // eventually, they will come from the API?
-const currCells = ['', '', 'o', 'o', 'x', 'x', 'x', 'o', '']
-const currPlayer = 'x'
 
 function Game (cells, currentPlayer) {
   this.cells = cells
   this.currentPlayer = currentPlayer
 }
 
-const currGame = new Game(currCells, currPlayer)
-
 module.exports = {
-  currGame
+  Game
 }
