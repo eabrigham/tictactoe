1280b5b459ad491dbcdc8619cd63df7292852af1
eabrigham <eabrigham@gmail.com>
2018-06-27 21:42:00 -0400

    Squashed bug where pressing 'New Game' multiple times created many boards.
    Used jquery .empty() on game-board div prior to
    creating new game.

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -3,7 +3,7 @@ const gameObj = require('./gameObj.js')
 
 const newGame = function (event) {
   event.preventDefault()
-
+  $('#game-board').empty()
   const blankBoard = ['', '', '', '', '', '', '', '', '']
   const currGame = new gameObj.Game(blankBoard)
   setup.boardToHTML(currGame)
