f2a4569534f1922671b81ebe8f4ab50d9530bf0d
eabrigham <eabrigham@gmail.com>
2018-07-01 13:37:07 -0400

    Added debugging console log to games/events.js

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -3,20 +3,18 @@ const gameObj = require('./gameObj.js')
 const ui = require('./ui.js')
 const api = require('./api.js')
 
-
 const newGame = function (event) {
   event.preventDefault()
   ui.wipeMessage()
   api.createGame()
     .then(function (data) {
+      console.log('Ran api create game and data is: ', data)
       ui.onCreateSuccess(data)
       const blankBoard = ['', '', '', '', '', '', '', '', '']
       const currGame = new gameObj.Game(blankBoard)
       setup.boardToHTML(currGame)
     })
     .catch(ui.onCreateFailure)
-
-
 }
 
 module.exports = {
