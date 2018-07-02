b956bba6fb4e0a51ded55f6aecea0fcb8fddbea9
eabrigham <eabrigham@gmail.com>
2018-07-02 18:13:12 -0400

    Made curl script JSON data single-line for debugging API

--- curl-scripts/update-game.sh
+++ curl-scripts/update-game.sh
@@ -18,13 +18,14 @@
 curl --include --request PATCH "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
   --header "Content-type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-  "game": {
-    "cell": {
-      "index": 0,
-      "value": "x"
-    },
-    "over": false
-  }
-}'
+  --data '{ "game": { "cell": { "index": 0, "value": "x" },"over": false } }'
+#   --data '{
+#   "game": {
+#     "cell": {
+#       "index": 0,
+#       "value": "x"
+#     },
+#     "over": false
+#   }
+# }'
 echo
