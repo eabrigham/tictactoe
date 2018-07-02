448d3ae74d8c8d9d17ecbf3aec76fa30423558c6
eabrigham <eabrigham@gmail.com>
2018-07-02 17:30:58 -0400

    Finally got working update curl script.
    Thanks to Aleks. Remember to use game ID not user ID.

--- curl-scripts/update-game.sh
+++ curl-scripts/update-game.sh
@@ -1,14 +1,30 @@
 # Script not curently working
-curl "https://tic-tac-toe-wdi.herokuapp.com/games/:${ID}" \
-  --include --request 'PATCH' \
-    --header 'content-type: application/json' \
-    --header "Authorization: Token token=${TOKEN}" \
-    --data '{
-      "game": {
-        "cell": {
-          "index": 0,
-          "value": "x"
-        },
-        "over": "false"
-      }
-    }'
+# curl "https://tic-tac-toe-wdi.herokuapp.com/games/:${ID}" \
+#   --include --request 'PATCH' \
+#     --header 'content-type: application/json' \
+#     --header "Authorization: Token token=${TOKEN}" \
+#     --data '{
+#       "game": {
+#         "cell": {
+#           "index": "0",
+#           "value": "x"
+#         },
+#         "over": "false"
+#       }
+#     }'
+
+
+# Aleks' script-- thanks!
+curl --include --request PATCH "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
+  --header "Content-type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+  "game": {
+    "cell": {
+      "index": "'"${INDEX}"'",
+      "value": "'"${VALUE}"'"
+    },
+    "over": "'"${OVER}"'"
+  }
+}'
+echo
