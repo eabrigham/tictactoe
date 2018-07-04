f9b697f8ba1ee83ce57d88bfd4a1d8db65b905a3
eabrigham <eabrigham@gmail.com>
2018-07-04 15:36:52 -0400

    Changed visibility of sign-in forms. WIP broke sign out.
    Need to fix the dom handling of emptying the game board div
    in order to fix sign out functionality.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,54 +3,49 @@
 const store = require('../store.js')
 
 const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully')
-  $('#message').css('background-color', 'green')
-  $('#new-game').css('visibility', 'visible')
+  $('#game-message').text('Signed up successfully')
   console.log('signUpSuccess ran. Data is :', data)
 }
 
 const signUpFailure = function (err) {
-  $('#message').text('Error on sign up.')
-  $('#message').css('background-color', 'red')
+  $('#game-message').text('Error on sign up.')
   console.log('signUpFailure ran. Data is :', err)
 }
 
 const signInSuccess = function (data) {
-  $('#message').text('Signed in successfully')
-  $('#message').css('background-color', 'green')
-  $('#new-game').css('visibility', 'visible')
+  $('#game-message').text('Signed in successfully')
+  $('#sign-up, #sign-in').css('visibility', 'hidden')
+  $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'visible')
   console.log('signInSuccess ran. Data is :', data)
   store.user = data.user
 }
 
 const signInFailure = function (err) {
-  $('#message').text('Error on sign in.')
-  $('#message').css('background-color', 'red')
+  $('#game-message').text('Error on sign in.')
   console.log('signInFailure ran. Error is :', err)
 }
 
 const changePasswordSuccess = function (data) {
-  $('#message').text('Changed password successfully')
-  $('#message').css('background-color', 'green')
+  $('#game-message').text('Changed password successfully')
   console.log('changePasswordSuccess ran and nothing was returned')
 }
 
 const changePasswordFailure = function (err) {
-  $('#message').text('Error on change password.')
-  $('#message').css('background-color', 'red')
+  $('#game-message').text('Error on change password.')
   console.log('changePasswordFailure ran. Error is :', err)
 }
 
 const signOutSuccess = function (data) {
-  $('#message').text('Signed out successfully')
-  $('#message').css('background-color', 'green')
+  $('#game-message').text('Signed out successfully')
+  $('#sign-up, #sign-in').css('visibility', 'visible')
+  $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'hidden')
+  document.getElementById('game-board').empty()
   console.log('signOutSuccess ran and nothing was returned')
   store.user = null
 }
 
 const signOutFailure = function (err) {
-  $('#message').text('Error on sign out.')
-  $('#message').css('background-color', 'red')
+  $('#game-message').text('Error on sign out.')
   console.log('signOutFailure ran. Error is :', err)
 }
 
