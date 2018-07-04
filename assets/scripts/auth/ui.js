f52be2cff6072f5fbd1d25f484bf32fc70320549
eabrigham <eabrigham@gmail.com>
2018-07-04 16:55:04 -0400

    Cleared UI forms after submission.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,16 +3,19 @@
 const store = require('../store.js')
 
 const signUpSuccess = function (data) {
+  document.getElementById('sign-up').reset()
   $('#game-message').text('Signed up successfully')
   console.log('signUpSuccess ran. Data is :', data)
 }
 
 const signUpFailure = function (err) {
+  document.getElementById('sign-up').reset()
   $('#game-message').text('Error on sign up.')
   console.log('signUpFailure ran. Data is :', err)
 }
 
 const signInSuccess = function (data) {
+  document.getElementById('sign-in').reset()
   $('#game-message').text('Signed in successfully')
   $('#sign-up, #sign-in').css('visibility', 'hidden')
   $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'visible')
@@ -21,21 +24,25 @@ const signInSuccess = function (data) {
 }
 
 const signInFailure = function (err) {
+  document.getElementById('sign-in').reset()
   $('#game-message').text('Error on sign in.')
   console.log('signInFailure ran. Error is :', err)
 }
 
 const changePasswordSuccess = function (data) {
+  document.getElementById('change-password').reset()
   $('#game-message').text('Changed password successfully')
   console.log('changePasswordSuccess ran and nothing was returned')
 }
 
 const changePasswordFailure = function (err) {
+  document.getElementById('change-password').reset()
   $('#game-message').text('Error on change password.')
   console.log('changePasswordFailure ran. Error is :', err)
 }
 
 const signOutSuccess = function (data) {
+  document.getElementById('sign-out').reset()
   $('#game-message').text('Signed out successfully')
   $('#sign-up, #sign-in').css('visibility', 'visible')
   $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'hidden')
@@ -45,6 +52,7 @@ const signOutSuccess = function (data) {
 }
 
 const signOutFailure = function (err) {
+  document.getElementById('sign-out').reset()
   $('#game-message').text('Error on sign out.')
   console.log('signOutFailure ran. Error is :', err)
 }
